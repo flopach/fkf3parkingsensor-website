f8bbf1065de8f96651623c98bc594d6d40532f37
Flo Pachinger <55385523+flopac@users.noreply.github.com>
2019-12-10 09:34:37 +0100

    new

--- js/getdata.js
+++ js/getdata.js
@@ -1,10 +1,10 @@
 function loadstatus() {
     $.ajax({
-    url: "http://127.0.0.1:5000/wc"
+    url: "https://fkf3parking.devnetcloud.com:5000/wc"
     }).then(function(data) {
         var json = JSON.parse(data);
         
-        if (json.park1 == 1) {
+        if (json.park1 == 0) {
             $('#park1').html('P1 is available!'); 
             $("#park1").css({"backgroundColor": "#81BC5A"});
         }
@@ -13,7 +13,7 @@ function loadstatus() {
             $("#park1").css({"backgroundColor": "#D1392C"});
         }
         
-        if (json.park2 == 1) {
+        if (json.park2 == 0) {
             $('#park2').html('P2 is available!'); 
             $("#park2").css({"backgroundColor": "#81BC5A"});
         }
@@ -22,7 +22,7 @@ function loadstatus() {
             $("#park2").css({"backgroundColor": "#D1392C"});
         }
         
-        if (json.park3 == 1) {
+        if (json.park3 == 0) {
             $('#park3').html('P3 is available!'); 
             $("#park3").css({"backgroundColor": "#81BC5A"});
         }
@@ -31,7 +31,7 @@ function loadstatus() {
             $("#park3").css({"backgroundColor": "#D1392C"});
         }
         
-        if (json.park4 == 1) {
+        if (json.park4 == 0) {
             $('#park4').html('P4 is available!'); 
             $("#park4").css({"backgroundColor": "#81BC5A"});
         }
